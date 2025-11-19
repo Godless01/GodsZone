f6577ad598d7f670eb85469861885821f5cf7eab
Godless01 <aresgwn@gmail.com>
2025-11-19 19:39:35 +0200

    Update SetupBanners.js

--- UI/banners/SetupBanners.js
+++ UI/banners/SetupBanners.js
@@ -1,8 +1,9 @@
 const setupBanners = {
-    helpBanner : 'https://i.ibb.co/Zz03cLsb/standard.gif',
+    helpBanner : 'https://i.ibb.co/LzHVJtrb/standard3-ezgif-com-effects.gif',
     verificationBanner : 'https://i.ibb.co/pjHDXzXx/static.png',
     ticketBanner : 'https://i.ibb.co/h1DBsRnv/ticket.gif',
     autovcBanner : 'https://i.ibb.co/mk6Tj1r/autovc.gif',
 };
 
 module.exports = setupBanners;
+
